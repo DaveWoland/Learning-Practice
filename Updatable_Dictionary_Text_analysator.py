38d89277eb2967858814eca0100cdf452d1cd8f9
David <davidwlangr@gmail.com>
2021-02-22 19:28:55 +0100

    Updated code to prevent overwriting already used 'key', using .setdefault() function.

--- Updatable_Dictionary_Text_analysator.py
+++ Updatable_Dictionary_Text_analysator.py
@@ -4,26 +4,25 @@ text1 = """In the past, I have been manually keying in the financial data for st
 text2 = """The above shows the entry point that prompts dialog to accept user’s input and further validate the input before calling the API to retrieve the stock price quote. The first prompts for the stock code, followed by the time series (whether it’s 1-min, 5-min, 15-min, hourly or daily) and the number of data points. As the number of data points that will be returned can go up to thousands and this will result in very slow Google Sheet, we are adding this field with a warning to indicate that high number can cause slowness."""
 
 
-
 def rozbor_vety(dictionary,text, order, character):
-    dct = {}
     lst = [slovo.strip(",.?!()") for slovo in text.split()]
-    for slovo in lst:
-        dct[f"text{order}"] = {
-                                'Words_count': sum(1 for slovo in lst if slovo.isalpha()),
-                                f"Words_count_started_with_{character}": len(slova_na_pismeno(lst, character)),
-                                f"List_of_words_started_with_{character}": [slovo for slovo in lst if slovo.startswith(character)],
-                                f"Count_of_same_words": {slovo: lst.count(slovo) for slovo in sorted(lst)}
-                                }
-    dictionary.update(dct)
+    dictionary.setdefault('Text'+str(order), {
+                                                "Words_count": sum(1 for slovo in lst if slovo.isalpha()),
+                                                f"Words_count_started_with_'{character}'": len(slova_na_pismeno(lst, character)),
+                                                f"List_of_words_started_with_'{character}'": [slovo for slovo in lst if slovo.lower().startswith(character)],
+                                                f"Count_of_same_words": {slovo: lst.count(slovo) for slovo in sorted(lst)}
+                                                }
+                          )
+
     return dictionary
 
 def slova_na_pismeno(lst, letter):
-    lst0 = [slovo for slovo in lst if slovo.startswith(letter)]
+    lst0 = [slovo for slovo in lst if slovo.lower().startswith(letter)]
     return lst0
 
 dct = {}
 rozbor_vety(dct, text1, 1, 't')
-rozbor_vety(dct, text2, 2, 'a')
-
 print(dct)
+rozbor_vety(dct, text2, 2, 'a')
+print(dct)
+
